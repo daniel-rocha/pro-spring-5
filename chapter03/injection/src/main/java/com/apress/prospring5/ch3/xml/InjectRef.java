6c1fe27bd0c2f89bc1b8af29a9ea53609e60a002
Daniel Rocha <dcrocha@gmail.com>
2019-04-15 22:38:08 +0800

    Finished injection part

--- chapter03/injection/src/main/java/com/apress/prospring5/ch3/xml/InjectRef.java
+++ chapter03/injection/src/main/java/com/apress/prospring5/ch3/xml/InjectRef.java
@@ -6,6 +6,7 @@ import com.apress.prospring5.ch3.Oracle;
 public class InjectRef {
     private Oracle oracle;
 
+    // this is going to be injected - see XML file.
     public void setOracle(Oracle oracle) { 
         this.oracle = oracle;
     }
@@ -15,6 +16,7 @@ public class InjectRef {
         ctx.load("classpath:spring/app-context-xml.xml");
         ctx.refresh();
 
+        // getting myself from spring, how weird
         InjectRef injectRef = (InjectRef) ctx.getBean("injectRef");
         System.out.println(injectRef);
 
